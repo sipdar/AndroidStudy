c263709b3c1140b811c4288f32c99e55ffaa3c86
sipdar <sipdar@163.com>
2016-06-03 10:51:38 +0800

    TestProject

---
+++ Test/app/src/main/java/com/example/sunny/test/DisplayMessageActivity.java
@@ -0,0 +1,27 @@
+package com.example.sunny.test;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        setContentView(textView);
+    }
+
+}
