499de4350d8dcc7ff77670eb9608d40a42a1f2a0
Sunny <sipdar@cuapp.me>
2016-06-07 12:53:18 +0800

    android model view controller

--- Test/app/src/main/java/com/example/sunny/test/MainActivity.java
+++ Test/app/src/main/java/com/example/sunny/test/MainActivity.java
@@ -6,43 +6,89 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.sunny.test.MESSAGE";
     private Button trueButton;
     private Button falseButton;
+    private Button nextButton;
+    private TextView questionView;
+    private Question[] mQuestions = new Question[]{
+        new Question(R.string.question_One, true),
+        new Question(R.string.question_Two, false),
+        new Question(R.string.question_three, false),
+        new Question(R.string.question_four, true),
+        new Question(R.string.question_five, true)
+    };
+
+    private int currentQuestionIndex;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         trueButton = (Button)findViewById(R.id.true_button);
-        falseButton= (Button)findViewById(R.id.false_button);
+        falseButton = (Button)findViewById(R.id.false_button);
+        nextButton = (Button)findViewById(R.id.next_button);
+        questionView = (TextView)findViewById(R.id.question_id);
+
         trueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, R.string.correct_toast,Toast.LENGTH_SHORT).show();
+                checkQuestionAnswer(true);
             }
         });
 
         falseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, R.string.inCorrect_toast,Toast.LENGTH_SHORT).show();
+                checkQuestionAnswer(false);
             }
         });
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateQuestionContent();
+            }
+        });
+
+
+        updateQuestionContent();
+    }
+
+    private void updateQuestionContent() {
+        int index = (currentQuestionIndex+ 1) % mQuestions.length;
+        Question question = mQuestions[index];
+        currentQuestionIndex = index;
+        questionView.setText(question.getTextResId());
+    }
+
+    private  void checkQuestionAnswer(boolean answerTrue) {
+        boolean questionIsTrue = mQuestions[currentQuestionIndex].isAnswerTrue();
+        int messageId = 0;
+        if (questionIsTrue == answerTrue) {
+            messageId = R.string.correct_toast;
+        } else {
+            messageId = R.string.inCorrect_toast;
+        }
+
+        Toast.makeText(MainActivity.this, messageId,Toast.LENGTH_SHORT).show();
     }
 
     public void trueButtonPressed(View view) {
-        Toast.makeText(MainActivity.this, R.string.correct_toast,Toast.LENGTH_SHORT).show();
+
 
     }
 
     public void  falseButtonPressed(View view) {
-        Toast.makeText(MainActivity.this, R.string.inCorrect_toast,Toast.LENGTH_SHORT).show();
 
     }
+
+
+
 //    public void sendMessage(View view) {
 //        Intent intent = new Intent(this, DisplayMessageActivity.class);
 //        EditText editText =  (EditText) findViewById(R.id.edit_message);
---
+++ Test/app/src/main/java/com/example/sunny/test/Question.java
@@ -0,0 +1,36 @@
+package com.example.sunny.test;
+
+/**
+ * Created by sunny on 16/6/7.
+ */
+
+public class Question {
+
+    private int mTextResId;
+
+    private boolean mAnswerTrue;
+
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        this.mAnswerTrue = answerTrue;
+    }
+
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        this.mTextResId = textResId;
+    }
+
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+}
